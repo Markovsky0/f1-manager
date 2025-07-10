136e9ed11108d65008c697502f9d0c2e60a8b70b
Marek.Paluch <marek.paluch@asseco.pl>
2025-07-10 09:15:29 +0200

    first commit

---
+++ src/main/java/pl/f1manager/F1ManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.f1manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1ManagerApplication.class, args);
+    }
+
+}
